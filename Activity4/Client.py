7facfa8eeff9e5577fa0ade11928625eb830f8c4
Ubuntu <ubuntu@ip-172-31-19-26.ec2.internal>
2021-03-21 18:40:21 +0000

    Pasos 1 - 5 completos

--- Activity4/Client.py
+++ Activity4/Client.py
@@ -1,15 +1,36 @@
 import socket
-from Crypto.Cipher import DES
-
-#byte or 64 bits
-key = b'thisis8b'
-iv = b'iv8bits!'
-# desInstance = DES.new(key, mode=DES.MODE_CBC, iv=iv)
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import  PKCS1_OAEP
+from Crypto.Cipher import  AES
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 plaintext = b'Hello!RSA-AES'
-# ciphertext = desInstance.encrypt(plaintext)
 s.connect(('54.226.141.110', 13337))
 s.send(plaintext)
-s.close()
+data = s.recv(1024)
+print(data.decode("ascii"))
 
+f=open("publickey.pem", "rb")
+key=RSA.import_key(f.read())
+f.close()
+pkcsInstance=PKCS1_OAEP.new(key)
+
+key=b'ayudenosprofe!!!'
+iv=b'profeayudenos!!!'
+key_iv= b'ayudenosprofe!!!,profeayudenos!!!'
+cipherkey_iv=pkcsInstance.encrypt(key_iv)
+s.send(key_iv)
+
+aesInstance=AES.new(key, AES.MODE_CBC, iv=iv)
+#aes_decryptInstance=AES.new(key, AES.MODE_CBC, iv)
+#aes_encryptInstance=AES.new(key, AES.MODE_CBC, iv)
+name_message=b'luisorevillayjesusgonzaleza!!!!!'
+aes_encrypt=aesInstance.encrypt(name_message)
+s.send(name_message)
+
+data2 = s.recv(1024)
+print(data2.decode("ascii"))
+aes_decrypt=aesInstance.decrypt(data2)
+print(aesInstance.decode("ascii"))
+
+s.close()
